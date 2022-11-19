ded26b6c9a6512fe236e2b6949427af04d93a99f
Naohiro Hamada <nao.pinguinus@icloud.com>
2022-11-19 17:03:04 +0900

    Do Lesson #1
    最初のコンポーズ可能関数の定義とプレビューの実装

--- jetpack_compose_tutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ jetpack_compose_tutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,41 +3,25 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Android")
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "Hello ${name}!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
